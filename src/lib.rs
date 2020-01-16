8b8c82a375a022ee6cf00159071c5a01fb3d03ba
cp_shen <cp_shen@outlook.com>
2020-01-16 16:37:47 +0800

    add triangle setup function

--- src/lib.rs
+++ src/lib.rs
@@ -18,20 +18,47 @@ pub fn perform_clipping(v2f_vec: &[V2f]) -> Vec<V2f> {
 
 pub fn perform_screen_mapping(
     v2f_vec: &[V2f],
-    fb: &Framebuffer,
+    width: usize,
+    height: usize,
 ) -> Vec<V2f> {
     v2f_vec
         .par_iter()
         .map(|v2f: &V2f| {
             let mut v2f_out = v2f.to_owned();
-            // TODO
+            v2f_out.pos.x *= width as f64 / 2.0;
+            v2f_out.pos.y *= height as f64 / 2.0;
             v2f_out
         })
         .collect()
 }
 
 pub fn setup_triangle(v2f_vec: &[V2f], indices: &[u32]) -> Vec<V2f> {
-    v2f_vec.to_owned()
+    assert_eq!(
+        indices.len() % 3,
+        0,
+        "indices length should be dividable by 3"
+    );
+
+    let v2f_out: Vec<V2f> = Vec::new();
+
+    indices
+        .par_iter()
+        .enumerate()
+        .filter_map(|(i, _)| {
+            if i % 3 == 0 {
+                Some((indices[i], indices[i + 1], indices[i + 2]))
+            } else {
+                None
+            }
+        })
+        .map(|(i0, i1, i2)| {
+            //TODO
+            let color = float4::new(0.0, 0.0, 0.0, 0.0);
+            let color = Some(color);
+            let pos = float3::new(0.0, 0.0, 0.0);
+            V2f { color, pos }
+        })
+        .collect()
 }
 
 pub fn process_fragments<FS>(v2f_vec: &[V2f], fs: FS) -> Vec<Fout>
--- tests/draw_tri.rs
+++ tests/draw_tri.rs
@@ -11,33 +11,46 @@ fn draw_tri() {
     ];
 
     let appdate: Vec<A2v> = positions
-        .iter()
+        .par_iter()
         .enumerate()
-        .step_by(3)
-        .map(|(i, _)| {
-            let vertex = float3::new(
-                positions[i],
-                positions[i + 1],
-                positions[i + 2],
-            );
-            A2v {
-                vertex,
-                normal: None,
-                texcoord: None,
-                texcoord1: None,
-                tangent: None,
-                color: None,
+        .filter_map(|(i, _)| {
+            if i % 3 == 0 {
+                let vertex = float3::new(
+                    positions[i],
+                    positions[i + 1],
+                    positions[i + 2],
+                );
+                let a2v = A2v {
+                    vertex,
+                    normal: None,
+                    texcoord: None,
+                    texcoord1: None,
+                    tangent: None,
+                    color: None,
+                };
+                Some(a2v)
+            } else {
+                None
             }
         })
         .collect();
 
     let tri_vs = |appdate: &A2v| {
         let pos = appdate.vertex;
-        let color = Some(float4::new(1.0, 1.0, 1.0, 1.0));
+        let color = None;
         V2f { pos, color }
     };
 
+    const SCR_WIDTH: usize = 800;
+    const SCR_HEIGHT: usize = 800;
+
     let v2f_vec = naivegl::process_vertices(&appdate, tri_vs);
     let v2f_vec = naivegl::perform_clipping(&v2f_vec);
-    //let v2f_vec = naivegl::perform_screen_mapping(&v2f_vec);
+    let v2f_vec = naivegl::perform_screen_mapping(
+        &v2f_vec, SCR_WIDTH, SCR_HEIGHT,
+    );
+
+    let indices: [u32; 3] = [0, 1, 2];
+
+    let v2f_vec = naivegl::setup_triangle(&v2f_vec, &indices);
 }
